0a2933e7df2d29f71173d8c8533910d53017ddf5
jhon <jhon041413@gmail.com>
2024-05-02 09:32:05 +0800

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Dashboard } from "./components/admin/pages/Dashboard";
+import { Login } from "./components/common/Login";
 
-function App() {
-  const [count, setCount] = useState(0)
+function AdminRoutes() {
+  const { pathname } = useLocation();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path={pathname} element={<Dashboard />}></Route>
+      <Route path={`${pathname}/../profile`} element={<Dashboard />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin/*" element={<AdminRoutes />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
