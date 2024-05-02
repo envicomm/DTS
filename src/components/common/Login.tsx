894a2f5f448dd212bf66f88b7059f9e4c73ab911
Envi Shy <envicomm.jay@gmail.com>
2024-05-02 11:21:25 +0800

    logodesign

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -5,8 +5,18 @@ export const Login = () => {
    
    
    <div className='min-h-screen w-screen grid grid-cols-2 gap-1'>
-      <div>Login</div>
-      <div>Login</div>
+      <div className="h-full w-full flex items-center justify-center p-4">
+        <img src ="frontlogo.png" alt="login" className="h-[420px] w-[680px] " />
+      </div>
+      <div className="h-full w-full flex flex-col p-4 items-center justify-center bg-gradient-to-t from-[#3DAA4A] gap-8">
+          
+            <div className="w-fullflex  justify-center">
+                <h1 className="font-semibold text-[40px]">Document Tracking System</h1>
+              </div>
+            <div className="flex w-[510px] h-[607px] bg-white shadow-2xl rounded-lg ">
+             
+            </div>
+      </div>
 
     </div>
   )
