4073d42dadefcaf76730045e5b9e75730e9c17d1
Envi Shy <envicomm.jay@gmail.com>
2024-05-02 09:44:32 +0800

    logindesign

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -4,6 +4,12 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div className="w-full bg-black">Login</div>
+   
+   
+   <div className='min-h-screen w-screen grid grid-cols-2 gap-1'>
+      <div>Login</div>
+      <div>Login</div>
+
+    </div>
   )
 }
